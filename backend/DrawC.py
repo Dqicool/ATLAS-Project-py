80470113c6824bb7ec5295ee375cd01b4b199bc3
Lewis Powell <lewisgpowell@gmail.com>
2019-07-23 17:01:49 +0100

    Allowed C++ code to search the same list of directories as python code

--- backend/DrawC.py
+++ backend/DrawC.py
@@ -1,19 +1,27 @@
 import ROOT as r
+from os import listdir
+from dataSets import dirs
 
 def DrawC(filename,lumStr,fast):
     """
     Function to load in the C++ code and run it for a given data set
     """
 
+    # search through several directories to find where the input file is located
+    for path in dirs:
+        if filename in listdir(path):
+            correctPath = path
+            break
+
     # reset environment and get path to file
     r.gROOT.Reset()
-    path = "/data/OpenData/" + filename 
+    fullPath = correctPath + filename 
 
     # load in CLoop.C
     r.gROOT.ProcessLine(".L backend/CLoop.C")
 
     # load in tree from file
-    r.gROOT.ProcessLine("TFile* f = new TFile(\""+path+"\")")
+    r.gROOT.ProcessLine("TFile* f = new TFile(\""+fullPath+"\")")
     r.gROOT.ProcessLine("TTree * minTree = new TTree")
     r.gROOT.ProcessLine("f->GetObject(\"mini\",minTree)")
 
