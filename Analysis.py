ecdda5d51f1df6432f4b4a832e15f09ad9d508d5
Lewis Powell <lewisgpowell@gmail.com>
2019-08-07 11:17:04 +0100

    Updated python example code

--- Analysis.py
+++ Analysis.py
@@ -4,7 +4,7 @@ import ROOT as r
 def Analyse(t,weighting):
 
     # Draw histograms
-    # Format is t.Draw("[variable to plot] >> [histogram name] ([number of bins], [x axis minimum], [x axis maximum]"),weighting)
+    # Format is t.Draw("variable_to_plot >> histogram_name (number_of_bins, x_axis_minimum, x_axis_maximum"),weighting)
 
     # Draw a histogram of number of leptons per event.
     t.Draw("lep_n >> h_lep_n(10, 0, 8)", weighting)
@@ -16,16 +16,17 @@ def Analyse(t,weighting):
     t.Draw("lep_etcone20/lep_pt >> h_lep_etnorm(100,-0.1,0.3)",weighting)
 
     # Selection cuts can be used to only plot data which passes certain tests.
-    # For second argument in t.Draw() use (weighting + "* [selection cuts]")
+    # For second argument in t.Draw() use (weighting + "* (selection_cuts)")
 
     # For example plotting transverse momentum of leptons for events with 3 or more leptons only:
     t.Draw("lep_pt >> h_3lep_pt(200,0,140e3)", weighting + "* (lep_n >= 3)")
 
 
     # It is also possible to plot calculated quantities.
+    # To make this easier one can use the function SetAlias("name","formula") to perform intermediate calculations
     # For example plotting the average transverse momenta of leptons in each event:
-    meanPt = "Sum$(lep_pt)/Length$(lep_pt)" 
-    t.Draw(meanPt + ">> h_lep_pt_mean(200,0,140e3)", weighting)
+    t.SetAlias("meanPt","Sum$(lep_pt)/Length$(lep_pt)") # define meanPt
+    t.Draw("meanPt >> h_lep_pt_mean(200,0,140e3)", weighting) # plot meanPt
     # For more information see https://root.cern.ch/doc/master/classTTree.html#a73450649dc6e54b5b94516c468523e45
 
 
@@ -43,6 +44,7 @@ def Analyse(t,weighting):
     h_lep_n.GetYaxis().SetTitle("Number of entries") # label y axis
     h_lep_n.SetTitle("Leptons per event")
     h_lep_n.SetLineColor(r.kRed) # set the line colour to red
+    # for more information on histogram styling see https://root.cern.ch/root/htmldoc/guides/users-guide/Histograms.html
 
 
     # Write histograms to the output file.
