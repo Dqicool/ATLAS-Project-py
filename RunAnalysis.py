106258f44cc90d5f8ca5255fd3d8e803172ddb60
Lewis Powell <lewisgpowell@gmail.com>
2019-08-07 16:11:41 +0100

    Cleaned up a bit

--- RunAnalysis.py
+++ RunAnalysis.py
@@ -8,8 +8,8 @@ import ROOT as r
 import os
 from Draw import Draw
 from DrawC import DrawC
+from getInput import getInput
 from infofile import infos
-from keyTranslate import keyTranslate
 from dataSets import dataSets, totRealLum, realList, dataCombos
 
 langMode = "C" # coding language to use for the analysis script
@@ -91,51 +91,14 @@ def combine(files, fast):
         hist.Write()
     totFile.Close()
 
-def getInput():
-    """
-    Function to get input list of decay chains from the user and verify them
-    Returns an array of keys to analyse and a boolean with their validity
-    """
-
-    # get list of decay chains from user
-    print("Please enter a comma-seperated list of decay chains.")
-    print("Use '+' to add data sets together.")
-    print("Write 'text' if you would prefer to read a list from 'input.txt':")
-    userInput = input() # get input
-
-    # check whether to read list from file or user input
-    if "text" in userInput: 
-        # read from file, remove newlines if present
-        with open("input.txt","r") as infile:
-            userInput = infile.read().replace("\n","")
-            
-    userInput = userInput.replace(" ","") # remove any spaces from list
-
-    series = userInput.split(",") # get list of series of chains from string
-
-    # new 2d list to hold chains
-    # first index is series, second is chain in the series
-    chains = [[]]*len(series)
-
-    for i in range(len(series)):
-       chains[i] = series[i].split("+") 
-
-    # check that all the decay chains are valid
-    chainsValid = True
-
-    for i in range(len(chains)):
-        for j in range(len(chains[i])):
-            if not ((chains[i][j] in dataSets.keys()) or (chains[i][j] in dataCombos.keys())):
-                print("Sorry I don't recognise " + chains[i][j] + " as a valid data set.")
-                chainsValid = False
-
-    # return the chains and their validity
-    return chains, chainsValid 
 
 # get input from user
 # keep asking until answered
 chainsValid = False
 while (not chainsValid):
+    print("Please enter a comma-seperated list of decay chains.")
+    print("Use '+' to add data sets together.")
+    print("Write 'text' if you would prefer to read a list from 'input.txt':")
     chains, chainsValid = getInput()
     print()
 
